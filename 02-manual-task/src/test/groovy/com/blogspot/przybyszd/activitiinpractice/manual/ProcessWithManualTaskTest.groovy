98e6b2eeee99a6dfd77ffbbe68e77c0667ad65e9
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-23 21:49:19 +0200

    Add module

---
+++ 02-manual-task/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/manual/ProcessWithManualTaskTest.groovy
@@ -0,0 +1,29 @@
+package com.blogspot.przybyszd.activitiinpractice.manual
+
+import org.activiti.engine.HistoryService
+import org.activiti.engine.RuntimeService
+import org.activiti.engine.history.HistoricProcessInstance
+import org.activiti.engine.runtime.ProcessInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@ContextConfiguration(locations = "/test-context.xml")
+class ProcessWithManualTaskTest extends Specification {
+
+    @Autowired
+    RuntimeService runtimeService
+
+    @Autowired
+    HistoryService historyService
+
+    @Unroll
+    def "should execute process with manual task"() {
+        when:
+            ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process-with-manual-task")
+        then:
+            HistoricProcessInstance result = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstance.processInstanceId).singleResult()
+            result.endTime != null
+    }
+}
