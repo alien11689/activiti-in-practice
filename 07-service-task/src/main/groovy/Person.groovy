5d654c77998814a3e0f0d202dd8d64d2ad5ce8a0
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-26 19:28:29 +0200

    Add test

---
+++ 07-service-task/src/main/groovy/Person.groovy
@@ -0,0 +1,9 @@
+package com.blogspot.przybyszd.activitiinpractice.servicetask
+
+import groovy.transform.Canonical
+
+@Canonical
+class Person implements Serializable {
+    String firstName
+    String lastName
+}
--- 07-service-task/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/servicetask/ServiceTaskTest.groovy
+++ 07-service-task/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/servicetask/ServiceTaskTest.groovy
@@ -1,10 +1,41 @@
 package com.blogspot.przybyszd.activitiinpractice.servicetask
 
+import org.activiti.engine.HistoryService
+import org.activiti.engine.RuntimeService
+import org.activiti.engine.history.HistoricVariableInstance
+import org.activiti.engine.runtime.ProcessInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+import spock.lang.Unroll
 
-/**
- * Created by alien on 4/26/14.
- */
-class ServiceTaskTest extends Specification{
-    
+@ContextConfiguration(locations = "/context.xml")
+class ServiceTaskTest extends Specification {
+
+    @Autowired
+    RuntimeService runtimeService
+
+    @Autowired
+    HistoryService historyService
+
+    @Unroll
+    def "should persist person"() {
+        when:
+            ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("add-person", ["username": "Piter Parker"])
+        then:
+            runtimeService.createProcessInstanceQuery().list() == []
+            List<HistoricVariableInstance> historicVariableInstances = historyService.createHistoricDetailQuery()
+                    .processInstanceId(processInstance.processInstanceId)
+                    .orderByVariableName()
+                    .asc()
+                    .list()
+            historicVariableInstances.size() == 3
+            historicVariableInstances.get(0).variableName == "username"
+            historicVariableInstances.get(0).value == "Piter Parker"
+            historicVariableInstances.get(0).variableName == "person"
+            historicVariableInstances.get(0).value == new Person("Piter","Parker")
+            historicVariableInstances.get(0).variableName == "id"
+            historicVariableInstances.get(0).value == 15l
+
+    }
 }
