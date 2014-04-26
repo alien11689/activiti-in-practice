0ce59cc6b4c5cfd2558a939d51ded69c9c8efd72
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-26 19:14:03 +0200

    Create module

---
+++ 07-service-task/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/servicetask/ServiceTaskTest.groovy
@@ -0,0 +1,10 @@
+package com.blogspot.przybyszd.activitiinpractice.servicetask
+
+import spock.lang.Specification
+
+/**
+ * Created by alien on 4/26/14.
+ */
+class ServiceTaskTest extends Specification{
+    
+}
