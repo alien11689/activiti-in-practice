e644829939437ecde3583d9e2d5aeeaae6342890
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-26 14:43:43 +0200

    Add test template

--- 04-variables-and-conditions/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/variablesandconditions/VariablesAndConditionsTest.groovy
+++ 04-variables-and-conditions/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/variablesandconditions/VariablesAndConditionsTest.groovy
@@ -1,11 +1,22 @@
 package com.blogspot.przybyszd.activitiinpractice.variablesandconditions
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Created by alien on 4/26/14.
  */
 class VariablesAndConditionsTest extends Specification{
 
-    
+    @Unroll
+    def "should go "() {
+        given:
+
+        when:
+
+        then:
+
+        where:
+
+    }
 }
