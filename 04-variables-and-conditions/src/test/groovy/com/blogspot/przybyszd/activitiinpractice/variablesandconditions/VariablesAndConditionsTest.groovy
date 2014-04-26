ec0812d3ca24830e12c86a89f0942a5c4dfa94b2
Dominik Adam Przybysz <alien11689@gmail.com>
2014-04-26 11:35:25 +0200

    Added template

---
+++ 04-variables-and-conditions/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/variablesandconditions/VariablesAndConditionsTest.groovy
@@ -0,0 +1,11 @@
+package com.blogspot.przybyszd.activitiinpractice.variablesandconditions
+
+import spock.lang.Specification
+
+/**
+ * Created by alien on 4/26/14.
+ */
+class VariablesAndConditionsTest extends Specification{
+
+    
+}
