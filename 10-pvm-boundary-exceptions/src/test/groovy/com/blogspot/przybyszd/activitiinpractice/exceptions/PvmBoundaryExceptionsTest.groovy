7e3c70409f9ca1c0f290c19bdb7bebd280956d4c
Dominik Adam Przybysz <alien11689@gmail.com>
2014-05-06 18:20:11 +0200

    Add context and empty class for tests

---
+++ 10-pvm-boundary-exceptions/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/exceptions/PvmBoundaryExceptionsTest.groovy
@@ -0,0 +1,9 @@
+package com.blogspot.przybyszd.activitiinpractice.exceptions
+
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(locations = "/context.xml")
+class PvmBoundaryExceptionsTest extends Specification{
+
+}
