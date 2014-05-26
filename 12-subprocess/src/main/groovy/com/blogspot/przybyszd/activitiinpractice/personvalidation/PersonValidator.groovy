fc97ca2c9c364fab08fa71046e14e734b893c275
Dominik Adam Przybysz <alien11689@gmail.com>
2014-05-26 20:47:06 +0200

    Fix validators

---
+++ 12-subprocess/src/main/groovy/com/blogspot/przybyszd/activitiinpractice/personvalidation/PersonValidator.groovy
@@ -0,0 +1,18 @@
+package com.blogspot.przybyszd.activitiinpractice.personvalidation
+
+import org.activiti.engine.delegate.BpmnError
+
+class PersonValidator {
+
+    void validateWithPossibleNull(String value) {
+        if (value != null && (value == "" || !Character.isUpperCase(value.charAt(0)))) {
+            throw new BpmnError("InvalidPerson")
+        }
+    }
+
+    void validateWithoutNull(String value) {
+        if (value == "" || !Character.isUpperCase(value.charAt(0))) {
+            throw new BpmnError("InvalidPerson")
+        }
+    }
+}
