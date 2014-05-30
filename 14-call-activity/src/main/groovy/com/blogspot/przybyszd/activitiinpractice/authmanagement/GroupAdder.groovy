44ab3641307f314388fabf499ba35234b3e593bf
Dominik Adam Przybysz <alien11689@gmail.com>
2014-05-30 21:35:16 +0200

    Add beans

---
+++ 14-call-activity/src/main/groovy/com/blogspot/przybyszd/activitiinpractice/authmanagement/GroupAdder.groovy
@@ -0,0 +1,10 @@
+package com.blogspot.przybyszd.activitiinpractice.authmanagement
+
+import org.springframework.stereotype.Component
+
+@Component
+class GroupAdder {
+    boolean add() {
+        true
+    }
+}
