260087ed87f6eb40ccac9220dd10d6fae24c9357
Dominik Adam Przybysz <alien11689@gmail.com>
2014-09-18 19:43:20 +0200

    Added draft of test

---
+++ 17-listeners/src/test/groovy/com/blogspot/przybyszd/activitiinpractice/listener/ProcessWithListenersTest.groovy
@@ -0,0 +1,53 @@
+package com.blogspot.przybyszd.activitiinpractice.listener
+
+import org.activiti.engine.EngineServices
+import org.activiti.engine.HistoryService
+import org.activiti.engine.RuntimeService
+import org.activiti.engine.TaskService
+import org.activiti.engine.delegate.event.ActivitiEvent
+import org.activiti.engine.delegate.event.ActivitiEventType
+import org.activiti.engine.history.HistoricProcessInstance
+import org.activiti.engine.history.HistoricVariableInstance
+import org.activiti.engine.runtime.ProcessInstance
+import org.activiti.engine.task.Task
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(locations = "/context.xml")
+class ProcessWithListenersTest extends Specification {
+
+    @Autowired
+    RuntimeService runtimeService
+
+    @Autowired
+    HistoryService historyService
+
+    @Autowired
+    TaskService taskService
+
+    def "should finished simplest process"() {
+        when:
+            ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("process-with-listeners")
+        then:
+            Task task = taskService.createTaskQuery().
+                    processInstanceId(processInstance.processInstanceId).
+                    taskName("enterGroupData").
+                    singleResult()
+            task != null
+        when:
+            taskService.claim(task.id, "test")
+            taskService.complete(task.id)
+        then:
+            HistoricProcessInstance result = historyService.createHistoricProcessInstanceQuery()
+                    .processInstanceId(processInstance.processInstanceId)
+                    .singleResult()
+            result.endTime != null
+            HistoricVariableInstance variable = historyService.createHistoricVariableInstanceQuery()
+                .processInstanceId(processInstance.processInstanceId)
+                .variableName("notifications")
+                .singleResult()
+            variable.value == ["task assign to test", "task completed", "group added", "process finished"]
+    }
+
+}
